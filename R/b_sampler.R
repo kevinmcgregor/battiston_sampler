7de5d599b76d6199ca2b93a948e29aab12cf7d9c
kevin.mcgregor@mail.mcgill.ca <kevin.mcgregor@d1p-hydratm01.ldi.lan>
2019-06-03 05:36:53 -0400

    added functions for sampling PY params

--- R/b_sampler.R
+++ R/b_sampler.R
@@ -4,25 +4,28 @@
 #' HPY sampler from Battiston et al. (2018)
 #'
 #' @param Y Matrix of taxa counts (rows are populations, columns are species)
-#' @param n.iter Number of MCMC iterations
+#' @param n.iter Number of MCMC iterations (after burn-in)
 #' @param quiet If TRUE, console output is suppressed
+#' @param n.burn Number of burn-in iterations
 #'
 #' @return
 #' @export
 #'
 #' @examples
-b_sampler <- function(Y, n.iter, quiet=FALSE) {
+b_sampler <- function(Y, n.iter, n.burn, quiet=FALSE) {
   if (!is.numeric(Y) | !is.matrix(Y) |
       any(Y<0) | any(Y!=floor(Y))) stop("Y must be a numeric matrix of positive counts")
   
   J <- NROW(Y)
   K <- NCOL(Y) # Number of distinct species in joint sample
   
-  # Initializing parameters
+  # Initializing PY parameters
   gamma <- 1 # Top-level concentation
-  sigma <- 0.5 <- # Top-level discount
-    
-  # Containers
+  alpha <- 0.5 # Top-level discount
+  theta <- rep(1, J) # Population-level concentration
+  sigma <- rep(0.5, J) # Population-level discount
+  
+  # Initializing table info
   n <- rowSums(Y) # The number of individuals in each population
   tab <- vector("list", length=J) # List to hold table indicators for each population
   for (j in 1:J) {
@@ -30,37 +33,97 @@ b_sampler <- function(Y, n.iter, quiet=FALSE) {
   }
   t.c <- vector("list", length=J) # Table counts (and species corresponding to each table)
   n.tab <- rep(0, J) # Number of tables in each population
+  n.s.tab <- matrix(0, J, K) # Number of tables for a given species in each population
   
+  gamma.s <- rep(0, n.iter)
+  alpha.s <- rep(0, n.iter)
+  theta.s <- matrix(0, n.iter, J)
+  sigma.s <- matrix(0, n.iter, J)
   
   # Loop over MCMC iterations
-  for (i in 1:n.iter) {
-    if (!quiet & i%%100==0) cat("Sample: ", i, "\n")
+  for (i in 1:(n.burn+n.iter)) {
+    if (!quiet & i==1) cat("Beginning burn-in:", "\n")
+    if (!quiet & i==n.burn+1) cat("Beginning sampling:", "\n")
+    if (!quiet & i%%100==0) cat("\t", i, "\n")
     
     # Loop over populations
     for (j in 1:J) {
-      
+      sp.vec <- rep(1:J, Y[j,])
       # Loop over individuals in a population
       for (p in 1:n[j]){
+        sp.cur <- sp.vec[p]
         # Remove current individual from its table
         t.cur <- tab[[j]][p]
         t.c[[j]][t.cur,] <- t.c[[j]][t.cur,] - 1
         if (t.c[[j]][t.cur,]==0) {
           # Drop table and make adjustments
           t.c[[j]] <- t.c[[j]][-t.cur,]
-          
+          tab[[j]][t.c[[j]]>t.cur] <- tab[[j]][t.c[[j]]>t.cur] - 1
+          n.tab[j] <- n.tab[j] - 1
+          n.s.tab[j, sp.cur] <- n.s.tab[j, sp.cur] - 1
         }
         
-        p.new <- 0.5
-        is.new <- rbinom(1, 1, p.new)
+        # Reassign individual to new or existing table
+        num <- (theta[j]+n.tab[j]*sigma[j])*(n.s.tab[j, sp.cur]-alpha)
+        den <- (gamma+sum(n.tab))*(sum(tab[[j]][tab[[j]][1,]==sp.cur,2])-n.s.tab[j, sp.cur]) +
+                                                  theta[j]+n.tab[j]*sigma[j]
+        is.new <- rbinom(1, 1, num/den)
         if (is.new) {
-          
+          # Allocate new table
+          t.c[[j]] <- rbind(t.c[[j]], c(sp.cur, 1))
+          n.tab[j] <- n.tab[j] + 1
+          n.s.tab[j, sp.cur] <- n.s.tab[j, sp.cur] + 1
+          tab[[j]][p] <- n.tab[j]
         } else {
-          
+          # Sample existing table (from proper species)
+          freq.t <- ifelse(t.c[[j]][,1]==sp.cur, (t.c[[j]][,2]-sigma[j])/(theta[j]+n[j]-1), 0)
+          wh.t <- sample(1:n[j], 1, prob=freq.t)
+          t.c[[j]][wh.t, 2] <- t.c[[j]][wh.t, 2] + 1
+          tab[[j]][p] <- wh.t
         }
       }
+      
+      # Sample local-level PY parameters
+      s.local <- py_local(theta[j], sigma[j])
+      theta[j] <- s.local$theta
+      sigma[j] <- s.local$sigma
+      
+      if (i>n.burn) {
+        theta.s[i,j] <- theta[j]
+        sigma.s[i,j] <- sigma[j]
+      }
+    }
+    
+    # Sample top-level PY parameters
+    s.top <- py_top()
+    gamma <- s.top$gamma
+    alpha <- s.top$alpha
+    
+    if (i>n.burn) {
+      gamma.s[i] <- gamma
+      alpha.s[i] <- alpha
     }
   }
   
-  return(1)
+  return(list(gamma=gamma.s, alpha=alpha.s, theta=theta.s, sigma=sigma.s,
+              tab=tab, t.c=t.c))
 }
+
+# TODO
+py_local <- function(theta.c, sigma.c) {
+  theta <- theta.c
+  sigma <- sigma.c
+  
+  return(list(theta=theta, sigma=sigma))
+}
+
+# TODO
+py_top <- function(gamma.c, alpha.c) {
+  gamma <- gamma.c
+  alpha <- alpha.c
+  
+  return(list(gamma=gamma, alpha=alpha))
+}
+
+
 
